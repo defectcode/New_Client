329787fadf4b88287ffd5c6ba212ae058e20ecbb
Constantin <paladuta05@gmail.com>
2024-12-06 16:58:42 +0200

    ref

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL, // Setează adresa backend-ului
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://vellov.com/api',
     withCredentials: true, // Include cookie-urile în cereri
 });
 
